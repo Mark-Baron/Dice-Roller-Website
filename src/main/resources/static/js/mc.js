d14cc3fc5d52cb73611e4d27a7a7bb5227c5b10b
Mark Baron <mark_baron@outlook.com>
2022-07-22 11:11:42 -0400

    my characters basic functionality

---
+++ src/main/resources/static/js/mc.js
@@ -0,0 +1,30 @@
+const API_BASE = 'http://localhost:8080/';
+let token = localStorage.getItem('token');
+
+document.addEventListener('DOMContentLoaded', () => {
+    getCharacters();
+});
+
+function getCharacters() {
+    fetch(API_BASE + 'characters', {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => response.json())
+        .then(json => {
+
+            if ('content' in document.createElement('template')) {
+                const main = document.getElementById('main');
+                json.forEach(character => {
+                    const tmpl = document.getElementById('character-template').content.cloneNode(true);
+                    tmpl.querySelector('h3').innerText = character.characterName;
+                    tmpl.querySelector('h4').innerText = character.characterLvl + " " + character.characterClass;
+
+                    main.appendChild(tmpl);
+                });
+            } else {
+                console.error('Your browser does not support templates');
+            }
+        });
+}
